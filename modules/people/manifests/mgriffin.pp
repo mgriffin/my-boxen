f1b3c8ef7584911f3da71671e8eb9e2dcb2ee1cf
Mike Griffin <griffinm@gmail.com>
2013-07-31 21:09:55 +0100

    Adds some specific installs for my set up

---
+++ modules/people/manifests/mgriffin.pp
@@ -0,0 +1,43 @@
+class people::mgriffin {
+
+  notify { 'class people::mgriffin declared': }
+
+  package { 'zsh':
+    ensure => installed,
+  }
+
+  # Changes the default shell to the zsh version we get from Homebrew
+  # Uses the osx_chsh type out of boxen/puppet-osx
+  osx_chsh { $::luser:
+    shell   => '/opt/boxen/homebrew/bin/zsh',
+    require => Package['zsh'],
+  }
+
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+
+  ##################################
+  ## Facter, Puppet, and Envpuppet##
+  ##################################
+
+  repository { "${::boxen_srcdir}/puppet":
+    source => 'puppetlabs/puppet',
+  }
+
+  repository { "${::boxen_srcdir}/facter":
+    source => 'puppetlabs/facter',
+  }
+
+  file { '/bin/envpuppet':
+    ensure  => link,
+    mode    => '0755',
+    target  => "${::boxen_srcdir}/puppet/ext/envpuppet",
+    require => Repository["${::boxen_srcdir}/puppet"],
+  }
+
+  include iterm2::stable
+  include alfred
+}
