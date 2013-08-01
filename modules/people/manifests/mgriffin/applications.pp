4154b6660e30bdf1f9db1c1be3273502fc314d81
Mike Griffin <griffinm@gmail.com>
2013-08-01 21:14:40 +0100

    Adds handbrake and vlc

--- modules/people/manifests/mgriffin/applications.pp
+++ modules/people/manifests/mgriffin/applications.pp
@@ -4,4 +4,11 @@ class people::mgriffin::applications {
   include alfred
   include dropbox
   include lastpass
+  include handbrake
+  include vlc
+
+  #package { 'libdvdcss':
+  #  source => 'http://download.videolan.org/libdvdcss/1.2.12/macosx/libdvdcss.pkg',
+  #  provider => pkg
+  #}
 }
