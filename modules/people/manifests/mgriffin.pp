a0b690aa1de9b6891334c42b010cc92c2628d627
Mike Griffin <griffinm@gmail.com>
2013-08-03 11:36:05 +0100

    Adds a plain zshrc file with nice defaults

--- modules/people/manifests/mgriffin.pp
+++ modules/people/manifests/mgriffin.pp
@@ -2,14 +2,25 @@ class people::mgriffin {
 
   $my_homedir = "/Users/${::luser}"
 
+  ## Global Settings
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
+
+  ## Dock Settings
+  include osx::dock::autohide
 
   include people::mgriffin::applications
 
   # My dotfile repository
   repository { "${my_homedir}/src/dotfiles":
     source => 'mgriffin/dotfiles',
+  }
+
+  file { "${my_homedir}/.zshrc":
+    ensure => link,
+    mode   => '0644',
+    target => "${my_homedir}/src/dotfiles/plain_zshrc",
+    require => Repository["${my_homedir}/src/dotfiles"],
   }
 
   file { "${my_homedir}/.vimrc":
